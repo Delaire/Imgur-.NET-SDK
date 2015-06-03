872ead6a58a6bbac4755eabac3a232dd55608142
Damien Delaire <delairedd@gmail.com>
2015-06-02 22:05:01 -0700

    ApiRoot

--- Imgur.API/Imgur.API/ApiRoot.cs
+++ Imgur.API/Imgur.API/ApiRoot.cs
@@ -2,10 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Imgur.API
 {
-    public class ApiRoot
+    public class ApiRoot : IApiRoot
     {
+        private ApiRoot()
+        {
+
+        }
+
+        private static ApiRoot _instance;
+
+        /// <summary>
+        /// The singleton ApiRoot instance
+        /// </summary>
+        public static ApiRoot Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ApiRoot();
+                }
+
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Returns TRUE if the SDK has been initialized. Initialize the SDK by calling Init().
+        /// </summary>
+        public bool IsInitialized { get; private set; }
+
+
+
+        public object AccessToken
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public void Init(string apiKey, string apiSecret)
+        {
+            //var authService = new AuthenticationService(apiKey, apiSecret);
+            ////authService.RefreshTokenAsync();
+            //authService.AccessTokenRefreshed += authService_AccessTokenRefreshed;
+
+            IsInitialized = true;
+        }
+
+        
+
+        public Task<object> AuthenticateAsync(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Imgur.API/Imgur.API/IApiRoot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imgur.API
+{
+    public interface IApiRoot
+    {
+        object AccessToken { get; set; }
+        //event ApiRoot.AccessTokenHandler AccessTokenRefreshed;
+
+        void Init(string apiKey, string apiSecret);
+        bool IsInitialized { get; }
+
+        Task<object> AuthenticateAsync(string username, string password);
+      
+    }
+}
