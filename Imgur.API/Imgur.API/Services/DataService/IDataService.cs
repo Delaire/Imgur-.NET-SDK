82a156cb8e4b529dee1ebc3addcb68826db98e32
Damien Delaire <delairedd@gmail.com>
2015-07-21 17:24:05 -0700

    Adding query

--- Imgur.API/Imgur.API/Services/DataService/IDataService.cs
+++ Imgur.API/Imgur.API/Services/DataService/IDataService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Imgur.API.EndPoints.Gallery;
 using Imgur.API.Model.Requests;
 
 namespace Imgur.API.Service.DataService
@@ -12,6 +13,8 @@ namespace Imgur.API.Service.DataService
         Task<T> MakeRequest<T>(RequestBase req);
 
         Task<T> GetEndPointEntityAsync<T>(RequestBase req);
+        Task<object> MakeQueryWithoutApiAuth<T>(RequestBase req);
+
         Task<TTarget> GetEndPointEntityAsync<TSource, TTarget>(RequestBase req, object Entity);
     }
 }
