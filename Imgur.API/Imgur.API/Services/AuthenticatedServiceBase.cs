9fe5f53cdf33b1f62c278b61b64fd7200d11fa65
Damien Delaire <delairedd@gmail.com>
2015-07-21 17:24:18 -0700

    Update

--- Imgur.API/Imgur.API/Services/AuthenticatedServiceBase.cs
+++ Imgur.API/Imgur.API/Services/AuthenticatedServiceBase.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Imgur.API.Ioc;
+using GalaSoft.MvvmLight.Ioc;
 using Imgur.API.Model;
 
 namespace Imgur.API.Service
