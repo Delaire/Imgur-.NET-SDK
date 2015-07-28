00abecbc15a67b861862cea6864cc0eb003afefa
Damien Delaire <delairedd@gmail.com>
2015-07-28 08:11:09 -0700

    adding file

---
+++ Imgur.API/Imgur.API/Model/Entities/DataCaption.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Imgur.API.Model.Entities
+{
+    public class DataCaption
+    {
+        public object image { get; set; }
+        public List<Caption> captions { get; set; }
+    }
+}
