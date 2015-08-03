8d8b2afc8b871c50e8283f40827ee26b29b7f9e8
Damien Delaire <delairedd@gmail.com>
2015-08-03 08:59:05 -0700

    updating file

--- Imgur.API/Imgur.API/EndPoints/Gallery/GetSearchGalleryImage.cs
+++ Imgur.API/Imgur.API/EndPoints/Gallery/GetSearchGalleryImage.cs
@@ -26,20 +26,5 @@ namespace Imgur.API.EndPoints.Gallery
             //get { return string.Format("gallery/{section}/{sort}/{window}/{page}?q=query",); }
             get { return string.Format("gallery/search/{0}/{1}/{2}?q={3}", sort, window, page, query); }
         }
-    }  public class GetGalleryTopicImage : RequestBase
-    {
- //Route		https://api.imgur.com/3/topics/{topic_id}/{sort}/{window}/{page}
-
-     
-        public string sort { get; set; }
-        public string window { get; set; }
-        public int page { get; set; }
-        public string topic_id { get; set; }
-
-        public override string CallIdentifier
-        {
-            //get { return string.Format("gallery/{section}/{sort}/{window}/{page}?q=query",); }
-            get { return string.Format("topics/{0}/{1}/{2}/{3}", topic_id, sort, window, page); }
-        }
     }
 }
