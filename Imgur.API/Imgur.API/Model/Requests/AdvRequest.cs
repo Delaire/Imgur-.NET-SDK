b84f7559231a3e7dc35a2eac998bdf337505c7eb
Damien Delaire <delairedd@gmail.com>
2015-08-12 08:08:40 -0700

    adding adv requests

---
+++ Imgur.API/Imgur.API/Model/Requests/AdvRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+
+namespace Imgur.API.Model.Requests
+{
+    public class AdvRequest : RequestBase
+    {
+        
+
+        public AdvRequest(HttpMethod method)  
+        {
+            Method = method;
+            //UrlToCall = urlToCall;
+        }
+
+        public override string CallIdentifier
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+    }
+}
---
+++ Imgur.API/Imgur.API/Model/Requests/RequestBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Imgur.API.Model.Requests
+{
+    public abstract class RequestBase
+    {
+        public HttpMethod Method { get; set; }
+
+        /// <summary>
+        /// Global Parameter
+        /// string get Url To Call
+        /// </summary>
+        public abstract string CallIdentifier { get; }
+
+        public bool IsLoginRequired { get; set; }
+
+        internal virtual string CacheKey
+        {
+            get
+            {
+                if (CallIdentifier != null)
+                {
+                    return String.Format("{0}:{1}", DateTime.Now.TimeOfDay.ToString(), CallIdentifier);
+                }
+
+                return "";
+            }
+        }
+
+
+    }
+}
