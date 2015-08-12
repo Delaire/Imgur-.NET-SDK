952692c55e2920393f63df32248d856c7fd2e4ed
Damien Delaire <delairedd@gmail.com>
2015-08-12 08:28:46 -0700

    adding post request

---
+++ Imgur.API/Imgur.API/Model/Requests/PostRequestBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Imgur.API.Model.Requests
+{
+    public abstract class PostRequestBase : RequestBase
+    {
+        /// <summary>
+        /// Global Parameter
+        /// string get post message To pass
+        /// </summary>
+        public abstract string CallPostMessage { get; }
+    }
+}
